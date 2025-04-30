a15f33fc2960dddc1eec57a09e491c72e55202d0
Manuel Luna <mluna881@proton.me>
2025-04-30 01:50:47 -0400

    hello

---
+++ cronos.sh
@@ -0,0 +1,130 @@
+#! /usr/bin/env bash
+
+SERVER_NAME="minecraft"
+BACKUP_DIR="$HOME/minecraft_backups"
+
+jobs_done() {
+	cat - <<EOF
+
+Job's done!
+  ／l、             
+（ﾟ､ ｡ ７         
+  l  ~ヽ       
+  じしf_,)ノ
+EOF
+}
+
+usage() {
+	cat - <<EOF
+Welcome!
+  ／l、             
+（ﾟ､ ｡ ７         
+  l  ~ヽ       
+  じしf_,)ノ
+
+EOF
+
+	echo "Usage:"
+	echo "- backup: creates a backup at this current time (stops server if its running)"
+	echo "$ cronos.sh backup"
+	echo
+
+	echo "Options:"
+	echo "--help | -h   show this message"
+
+	exit 1
+}
+
+[ -z "$1" ] && usage
+
+backup() {
+	if [ ! -d "$BACKUP_DIR" ]; then
+		echo -n "Creating backup directory: ${BACKUP_DIR}... "
+		mkdir -p "$BACKUP_DIR" || exit
+		echo "OK."
+	fi
+
+	fabric_pid=$(pgrep -f fabric-server)
+	should_restart=0
+	if [ -z "$fabric_pid" ]; then
+		echo "Server is not running. OK."
+	else
+		if ! tmux info &>/dev/null; then
+			echo "Server is running outside of tmux! Please run it inside a tmux session." >&2
+			exit 1
+		fi
+
+		tmux_pane_id=$(tmux list-panes -a -F '#{pane_id} #{pane_current_command}' | grep -i "java" | awk '{print $1}')
+		if [ -z "$tmux_pane_id" ]; then
+			echo "Server is running outside of tmux! Please run it inside a tmux session." >&2
+			echo "Tip: this can also mean you wrapped your server start command in a script but did not use exec"
+			exit 1
+		fi
+		pids_found=$(wc -l <<< "$tmux_pane_id")
+		if (( pids_found > 1 )); then
+			echo "More than 1 tmux pane running java. This script expect only one pane with java running (the server)." >&2
+			exit 1
+		fi
+			
+		should_restart=1
+		msg="$(date +'%H:%M:%S') SERVER IS RESTARTING FOR A BACKUP. SEE YOU ON THE OTHER SIDE!"
+		tmux send-keys -t "$tmux_pane_id" say Space "$msg" Enter || exit
+		tmux send-keys -t "$tmux_pane_id" save-off Enter || exit
+		tmux send-keys -t "$tmux_pane_id" save-all Space flush Enter || exit
+
+		seconds_passed=0
+		echo -n "Waiting for server to pause... "
+		while true; do
+			if (( seconds_passed > 600 )); then
+				echo "I've waited for 10 minutes but server does not seem to respond. Aborting." >&2
+				exit 1
+			fi
+
+			if tmux capture-pane -p -t "$tmux_pane_id" | grep -A 5000 "$msg" | grep -q "Saved the game"; then
+				echo "OK."
+				break
+			fi
+
+			sleep 1
+			seconds_passed=$(( seconds_passed + 1 ))
+		done
+	fi
+
+	timestamp=$(date +"%F_%H-%M-%S") || exit
+	backup_name="${SERVER_NAME}_${timestamp}.tar.gz"
+	echo -n "Saving ${backup_name} to ${BACKUP_DIR}... "
+	err=$(tar --force-local -cvzf "${BACKUP_DIR}/${backup_name}" . 2>&1)
+	if [ "$?" -ne 0 ]; then
+		if [ "$should_restart" -eq 1 ]; then
+			tmux send-keys -t "$tmux_pane_id" save-on Enter
+		fi
+		echo "$err"
+		exit 1
+	fi
+	echo "OK."
+	if [ "$should_restart" -eq 1 ]; then
+		tmux send-keys -t "$tmux_pane_id" save-on Enter || exit
+	fi
+	jobs_done
+}
+
+while true; do
+	[ -z "$1" ] && break
+
+	case "$1" in
+		--help|-h)
+			shift
+			usage
+			;;
+
+		backup)
+			shift
+			backup
+			;;
+
+		*)
+			shift
+			usage
+			;;
+	esac
+done
